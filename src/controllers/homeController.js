2db094f70361c8c72279c7328b386d1ec1fd1e89
augustustung <huytung139@gmail.com>
2022-04-15 11:18:30 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -18,45 +18,45 @@ let getHomePage = async (req, res) => {
     return res.render('homePage.ejs')
 }
 
-let postWebHook = (req, res) => {  
+let postWebHook = (req, res) => {
     let body = req.body;
 
     // Checks this is an event from a page subscription
     if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function (entry) {
 
-        // Gets the body of the webhook event
-        let webhook_event = entry.messaging[0];
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
 
-        // Get the sender PSID
-        let sender_psid = webhook_event.sender.id;
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
 
-        // Check if the event is a message or postback and
-        // pass the event to the appropriate handler function
-        if (webhook_event.message) {
-            handleMessage(sender_psid, webhook_event.message);
-        } else if (webhook_event.postback) {
-            handlePostback(sender_psid, webhook_event.postback);
-        }
-    });
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+        });
 
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
+        // Returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
     } else {
         console.log('errr post webhook');
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
     }
-  
+
 }
 
 let getWebHook = (req, res) => {
 
     // Your verify token. Should be a random string.
     let VERIFY_TOKEN = process.env.VERIFY_TOKEN
-      
+
     // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
@@ -64,18 +64,18 @@ let getWebHook = (req, res) => {
 
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-        
+
         // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
 
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
 }
 
@@ -199,8 +199,6 @@ function callSendAPI(sender_psid, response) {
         }
     }
 
-    console.log('res', response);
-
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v13.0/me/messages",
@@ -209,6 +207,7 @@ function callSendAPI(sender_psid, response) {
         "json": request_body
     }, (err) => {
         if (!err) {
+            console.log(request_body);
             console.log('message sent!')
         } else {
             console.error("Unable to send message:" + err);
@@ -299,16 +298,16 @@ let handleBooking = (req, res) => {
 
 let handleBookingSchedule = async (req, res) => {
     try {
-        let getNameFromFacebook = await chatbotService.getUserName(req.body.psid)
+        // let getNameFromFacebook = await chatbotService.getUserName(req.body.psid)
 
         let customerName = req.body.customerName
-        if (req.body.customerName === "") {
-            customerName = getNameFromFacebook
-        }
+        // if (req.body.customerName === "") {
+        //     customerName = getNameFromFacebook
+        // }
 
         //write data to google sheet
         const data = {
-            username: getNameFromFacebook,
+            username: customerName,
             customerName: customerName,
             phoneNumber: req.body.phoneNumber,
             email: req.body.email,
